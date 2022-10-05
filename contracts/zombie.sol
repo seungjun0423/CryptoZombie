72a938395f46d89868a849e27e5538b69e97d2ff
seungjun0423 <sibonum@gmail.com>
2022-10-06 08:24:13 +0900

    Chapter 1

--- contracts/zombie.sol
+++ contracts/zombie.sol
@@ -1,1 +1,19 @@
 pragma solidity ^0.8.17;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function createZombie(string _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+
+}
