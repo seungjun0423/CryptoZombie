4c7d4082a6fa479beb7309b9d6d4954f792ff443
seungjun0423 <sibonum@gmail.com>
2022-10-16 12:10:16 +0900

    fix TypeError

--- contracts/zombie.sol
+++ contracts/zombie.sol
@@ -12,7 +12,7 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function createZombie(string _name, uint _dna) private {
+    function createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
     }
 
